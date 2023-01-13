5e4ce896a7bd6893e21b1b48aed8f24baa194549
Avdhesh Chaudhary <avdheshchaudhary00100@gmail.com>
2023-01-14 00:23:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/wobot-logo.svg";
+import solidlines from "./assets/Solid_lines.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <img src={logo} alt='img'/>
+      </div>
+      <div className="main">
+        <div className='rect'></div>
+        <div className="form-container">
+          <img src={solidlines} alt='img'/>
+          <p className="p1">It's a delight to have you<br/>onboard</p>
+          <p className="p2">Help us know you better.<br/>
+            (This is how we optimize Wobot as per your business needs)
+          </p>
+          <form>
+            <label htmlFor='cname'>Company name</label>
+            <input type="text" id="cname" placeholder="e.g. Example Inc" />
+
+            <label htmlFor='industry'>Industry</label>
+            <select id="industry" name="industry">
+              <option>Select</option>
+              <option value="IT">Information Technology</option>
+              <option value="Textiles">Textiles</option>
+              <option value="Chemicals">Chemicals</option>
+            </select>
+
+            <label>Company Size</label>
+            <div className='compSize'>
+              <div>1-20</div>
+              <div>21-50</div>
+              <div>51-200</div>
+              <div>201-500</div>
+              <div>500+</div>
+            </div>
+            <input type='submit' value='Get Started'/>
+          </form>
+        </div>
+        <div className='rect'></div>
+      </div>
+      <div className='footer'>
+        <p className='content'>Terms of use | Privacy policy</p>
+      </div>
     </div>
   );
 }
